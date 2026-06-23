0b8f162faa7b2532523a4229e4d9a9aae06a50de
Emmanuel Makoye <makoye@Emmanuels-MacBook-Air.local>
2026-06-22 20:38:41 -0700

    fix image issues

--- next.config.js
+++ next.config.js
@@ -4,15 +4,9 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: '**' },
     ],
-    domains: [
-      'd2bstvyam1bm1f.cloudfront.net',
-      'd3qiuw9agheakm.cloudfront.net',
-      'images.unsplash.com',
-    ],
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 640],
-    qualities: [75, 85, 95],
   },
 };
 
